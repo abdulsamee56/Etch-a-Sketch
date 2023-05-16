118263b1c2defac0591d0176d3ebeeeda243fc94
abdulsamee56 <sameeabdul56@gmail.com>
2023-05-16 13:03:29 -0400

    Added functions for the grid layout and dom

---
+++ index.js
@@ -0,0 +1,27 @@
+function PopulateBoard(size){
+    let container = document.querySelector('.container');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div)=>div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+
+    let amount = size*size;
+    for (i =0; i < amount;i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'blue';
+        container.insertAdjacentElement('beforeend',square);
+    
+    }
+}
+
+PopulateBoard(16);
+
+function changeSize(input){
+    
+    if(input >=2 && input <= 100){
+    PopulateBoard(input);}
+    else{
+        alert("Squares must be between 2-100");
+    }
+}
